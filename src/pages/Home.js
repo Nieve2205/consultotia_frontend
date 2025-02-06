10d7e180debd38f1bc94233ba3705dd230f028ba
Nieve2205 <ashleepre2022@gmail.com>
2025-02-05 21:41:57 -0500

    Ofertas en el Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../Home.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
